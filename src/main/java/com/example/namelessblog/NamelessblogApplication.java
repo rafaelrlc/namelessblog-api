9ec4acaee60ae6c4c17a11a650877e7cdbc35fb5
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-09 15:39:07 -0300

    feat : first commit

---
+++ src/main/java/com/example/namelessblog/NamelessblogApplication.java
@@ -0,0 +1,14 @@
+package com.example.namelessblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamelessblogApplication {
+
+	public static void main(String[] args) {
+		System.out.println("First-Commit");
+		SpringApplication.run(NamelessblogApplication.class, args);
+	}
+
+}
