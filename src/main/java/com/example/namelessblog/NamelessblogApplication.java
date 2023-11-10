1e3dd9e369989d46564c0163e509be01e56d70fe
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-10 01:33:57 -0300

    feat : postgres connection + testes

--- src/main/java/com/example/namelessblog/NamelessblogApplication.java
+++ src/main/java/com/example/namelessblog/NamelessblogApplication.java
@@ -1,13 +1,27 @@
 package com.example.namelessblog;
 
+import com.example.namelessblog.domain.entity.User;
+import com.example.namelessblog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NamelessblogApplication {
 
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired UserRepository users) {
+		return args -> {
+			User user1 = new User("Rafael", "rafael@gmail.com", "123123", "RafaelRibeiro");
+			users.save(user1);
+
+
+		};
+	}
+
 	public static void main(String[] args) {
-		System.out.println("First-Commit");
 		SpringApplication.run(NamelessblogApplication.class, args);
 	}
 
