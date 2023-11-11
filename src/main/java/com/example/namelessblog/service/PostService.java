f7099006dd5ba3e6869a94867f7ba774cf1577a1
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-11 17:56:19 -0300

    feat : add savePost and deletePost method

--- src/main/java/com/example/namelessblog/service/PostService.java
+++ src/main/java/com/example/namelessblog/service/PostService.java
@@ -1,6 +1,10 @@
 package com.example.namelessblog.service;
 
+import com.example.namelessblog.rest.dto.PostDTO;
 import org.springframework.stereotype.Repository;
 
 public interface PostService {
+
+    void deletePostById(Long id, Long userId);
+    void savePost(PostDTO dto);
 }
--- src/main/java/com/example/namelessblog/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/namelessblog/service/impl/PostServiceImpl.java
@@ -1,16 +1,49 @@
 package com.example.namelessblog.service.impl;
 
+import com.example.namelessblog.domain.entity.Post;
+import com.example.namelessblog.domain.entity.User;
 import com.example.namelessblog.repository.PostRepository;
+import com.example.namelessblog.repository.UserRepository;
+import com.example.namelessblog.rest.dto.PostDTO;
 import com.example.namelessblog.service.PostService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Date;
+import java.util.Objects;
 
 @Service
-@AllArgsConstructor
 public class PostServiceImpl implements PostService {
 
-    private final PostRepository postRepository;
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private UserRepository userRepository;
 
+    public PostServiceImpl(PostRepository postRepository, UserRepository userRepository) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
 
+    public void savePost(PostDTO dto) {
+        User user = userRepository.findById(dto.getUserId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+        Date date = new Date();
+        Post post = new Post(dto.getTitle(), dto.getContent(), user, date);
+        postRepository.save(post);
+    }
+
+    public void deletePostById(Long id, Long userId) {
+        // futuramente o user vai ser pego pelo token e não pelo id passado na requisição
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found"));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        if (!Objects.equals(post.getAuthor().getId(), user.getId())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Failed to delete post");
+        }
+
+        postRepository.deleteById(id);
+    }
 
 }
