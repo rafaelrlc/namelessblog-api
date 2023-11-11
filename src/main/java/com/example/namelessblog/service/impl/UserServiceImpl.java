7dfea8e0279259b0ecb580562a17791e8c09af43
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-11 18:02:59 -0300

    feat : D.I userRepository on userService layer

--- src/main/java/com/example/namelessblog/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/namelessblog/service/impl/UserServiceImpl.java
@@ -1,16 +1,23 @@
 package com.example.namelessblog.service.impl;
 
+import com.example.namelessblog.repository.PostRepository;
 import com.example.namelessblog.repository.UserRepository;
 import com.example.namelessblog.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
-@RequiredArgsConstructor
+
 public class UserServiceImpl implements UserService {
 
+    @Autowired
     private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
 }
