06232cb193510ea4f7188c53e2a771002e914245
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-13 01:01:16 -0300

    feat : add findUserById and saveUser method to UserService

--- src/main/java/com/example/namelessblog/service/UserService.java
+++ src/main/java/com/example/namelessblog/service/UserService.java
@@ -1,6 +1,9 @@
 package com.example.namelessblog.service;
 
+import com.example.namelessblog.domain.entity.User;
 import org.springframework.stereotype.Repository;
 
 public interface UserService {
+    User findUserById(Long id);
+    void saveUser(User user);
 }
--- src/main/java/com/example/namelessblog/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/namelessblog/service/impl/UserServiceImpl.java
@@ -1,16 +1,18 @@
 package com.example.namelessblog.service.impl;
 
+import com.example.namelessblog.domain.entity.User;
 import com.example.namelessblog.repository.PostRepository;
 import com.example.namelessblog.repository.UserRepository;
 import com.example.namelessblog.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @Service
-
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -20,4 +22,13 @@ public class UserServiceImpl implements UserService {
         this.userRepository = userRepository;
     }
 
+    @Override
+    public User findUserById(Long id) {
+        return this.userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+    }
+
+    @Override
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
 }
