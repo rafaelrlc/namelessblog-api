27220d57f588e7a2f97bd4db3352007069f896c3
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-13 01:03:47 -0300

    feat : add create new user route

--- src/main/java/com/example/namelessblog/rest/controller/UserController.java
+++ src/main/java/com/example/namelessblog/rest/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.example.namelessblog.rest.controller;
 
+import com.example.namelessblog.domain.entity.User;
 import com.example.namelessblog.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +15,14 @@ public class UserController {
 
     private final UserService userService;
 
-
+    
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> newUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
