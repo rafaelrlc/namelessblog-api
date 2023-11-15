98cd2b46eb2344c144628487408b25db7e3b2344
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-15 16:16:48 -0300

    update

--- src/main/java/com/example/namelessblog/NamelessblogApplication.java
+++ src/main/java/com/example/namelessblog/NamelessblogApplication.java
@@ -1,7 +1,9 @@
 package com.example.namelessblog;
 
+import com.example.namelessblog.domain.entity.Badge;
 import com.example.namelessblog.domain.entity.Post;
 import com.example.namelessblog.domain.entity.User;
+import com.example.namelessblog.repository.BadgeRepository;
 import com.example.namelessblog.repository.PostRepository;
 import com.example.namelessblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,24 +17,48 @@ import java.util.Date;
 @SpringBootApplication
 public class NamelessblogApplication {
 
-	@Bean
-	public CommandLineRunner commandLineRunner(@Autowired UserRepository users , @Autowired PostRepository posts) {
-		return args -> {
-			User user1 = new User("Rafael", "rafael@gmail.com", "123123", "RafaelRibeiro");
-			users.save(user1);
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired UserRepository users, @Autowired PostRepository posts, @Autowired BadgeRepository badges) {
+        return args -> {
+            User user1 = new User("Rafael", "rafael@gmail.com", "123123", "RafaelRibeiro");
+            users.save(user1);
 
-			Post post1 = new Post();
-			post1.setTitle("Primeiro post");
-			post1.setContent("Conteúdo do primeiro post");
-			post1.setAuthor(user1);
-			post1.setDate(new Date());
-			posts.save(post1);
+            Post post1 = new Post();
+            post1.setTitle("Primeiro post");
+            post1.setContent("Conteúdo do primeiro post");
+            post1.setAuthor(user1);
+            post1.setDate(new Date());
+            posts.save(post1);
 
-		};
-	}
+            Post post2 = new Post();
+            post2.setTitle("2 post");
+            post2.setContent("Conteúdo do 2 post");
+            post2.setDate(new Date());
+            post2.setAuthor(user1);
+            posts.save(post2);
 
-	public static void main(String[] args) {
-		SpringApplication.run(NamelessblogApplication.class, args);
-	}
+
+            Badge badge1 = new Badge("Art", "yellow");
+            Badge badge2 = new Badge("Software", "blue");
+
+            badges.save(badge1);
+            badges.save(badge2);
+
+            post2.getBadges().add(badge1);
+            post2.getBadges().add(badge2);
+
+            posts.save(post2);
+
+
+
+
+
+
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamelessblogApplication.class, args);
+    }
 
 }
