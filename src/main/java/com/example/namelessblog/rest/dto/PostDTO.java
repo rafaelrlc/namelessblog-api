867639f62724544d28436be0d042d15dd883f25f
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-12-29 21:34:11 -0300

    update

--- src/main/java/com/example/namelessblog/rest/dto/PostDTO.java
+++ src/main/java/com/example/namelessblog/rest/dto/PostDTO.java
@@ -3,6 +3,6 @@ package com.example.namelessblog.rest.dto;
 
 import java.util.Set;
 
-public record PostDTO (String title, String content, Long userId, Set<Long> badgesId) {
+public record PostDTO(String title, String content, Long userId, Set<Long> tagsId) {
 
 }
--- src/main/java/com/example/namelessblog/rest/dto/UpdatePostDTO.java
+++ src/main/java/com/example/namelessblog/rest/dto/UpdatePostDTO.java
@@ -2,5 +2,5 @@ package com.example.namelessblog.rest.dto;
 
 import java.util.Set;
 
-public record UpdatePostDTO(String title, String content, Long userId, Long postId) {
+public record UpdatePostDTO(String title, String content, Long userId, Long postId, Set<Long> tagsId) {
 }
